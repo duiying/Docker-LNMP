c4171c136fe43fd4999d216d457b76d1fbb86910
wyx <1822581649@qq.com>
2019-03-12 10:37:53 +0800

    fix:crontab无法执行

--- docker/files/cgi/docker-entrypoint.sh
+++ docker/files/cgi/docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # @auther <1822581649@qq.com>
 
+/usr/sbin/init
+/usr/sbin/crond
 echo "info: starting php-fpm.."
 /usr/sbin/php-fpm -c /etc/php.ini -y /etc/php-fpm.conf
 tail -f /dev/null
